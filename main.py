4c01cf2a6c667affcf0912502398274ea0f94f94
Wraithlin1984 <zldavies@hotmail.com>
2025-08-05 12:48:54 +0100

    Version 1: - Grabs data for (most recent date) - Saves locally - Plots a timeseries - Performs frquency analysis on values and digits - Plots two more charts
    2.0
    - Read API key from locally stored text file
    - As the user for a series ID
    -- Added some error handling for bad ID's and other HTTP error codes.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #The Main
 
 from modules.import_api_key import import_api_key
-from modules.fetch_data import download_us_yields
+from modules.fetch_data import download_FRED_data
 from modules.ingest_data import load_data_from_json
 from modules.plot_data import plot_data_timeseries
 from modules.frequency_distribution import bucket_timeseries
@@ -12,16 +12,23 @@ def main():
     #Grab my FRED API key from local
     API_key = import_api_key()
 
-    #Download, save as local, and ingest the data
-    json_file = download_us_yields(API_key)
+    #Download, save as local
+    series_id = input("Which FRED data would you like to download? (Series ID): ")
+    json_file = download_FRED_data(API_key, series_id)
+    #Added error handling for bad series_id
+    if json_file is None:
+        print("Data ingestion failed")
+        return()
     print("File saved successfully to: {json_file}")
 
+    #Now ingest the data for manipulation
     dataseries = load_data_from_json(json_file)
     print("Data series loaded successfully.")
 
     #Plot a graph of the timeseries
+    #Note to self: Could we read the series name from the FRED file, and pass that title.
     plot_data_timeseries(dataseries, 10,5)
-    input("Timeseries of 10y UST yields. Press enter to continue...")
+    input(f"Timeseries {series_id}. Press enter to continue...")
 
     #Frequency analysis 1: Distribution of yields
     bucket_size = 0.25
